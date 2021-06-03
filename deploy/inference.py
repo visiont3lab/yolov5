42638219041bbbe028e22b12f601cb063aab623f
visionlab <visiont3lab@gmail.com>
2021-06-03 12:07:11 +0200

    added class yolov5 inference

--- deploy/inference.py
+++ deploy/inference.py
@@ -57,22 +57,99 @@ def non_max_suppression_fast(boxes, overlapThresh):
     # integer data type
     return boxes[:,pick].astype("int"), pick
 
-class_names = ['dado-M3', 'dado-M5', 'dado-M6', 'dado-M8', 'rondella-M3', 'rondella-M6', 'vite-croce', 'vite-esagonale', 'vite-taglio-grande', 'vite-taglio-piccola']
-class_colors =  [(125,0,0), (125,0,50), (125,100,0), (125,0,125),(0,255,0), (255,255,0), (125,0,255), (125,255,255), (0,0,255), (125,0,65)]
-conf = 0.8
 
-sess = rt.InferenceSession("models/best.onnx")
-input_name = sess.get_inputs()[0].name
-label_name = sess.get_outputs()[0].name
-input_shapes = sess.get_inputs()[0].shape
-output_shapes = sess.get_outputs()[0].shape
-c, h ,w = (input_shapes[1],input_shapes[2],input_shapes[3])
+class Inference:
+    def __init__(self):
+        self.class_names = ['dado-M3', 'dado-M5', 'dado-M6', 'dado-M8', 'rondella-M3', 'rondella-M6', 'vite-croce', 'vite-esagonale', 'vite-taglio-grande', 'vite-taglio-piccola']
+        self.class_colors =  [(125,0,0), (125,0,50), (125,100,0), (125,0,125),(0,255,0), (255,255,0), (125,0,255), (125,255,255), (0,0,255), (125,0,65)]
+        self.conf = 0.8
 
-print(f"input name: {input_name}")
-print(f"input shapes: {input_shapes}")
+        self.sess = rt.InferenceSession("models/best.onnx")
 
-print(f"output name: {label_name}")
-print(f"output shapes: {output_shapes}")
+        self.input_name = self.sess.get_inputs()[0].name
+        self.label_name = self.sess.get_outputs()[0].name
+        input_shapes = self.sess.get_inputs()[0].shape
+        output_shapes = self.sess.get_outputs()[0].shape
+        print(f"input name: {self.input_name}")
+        print(f"input shapes: {input_shapes}")
+        print(f"output name: {self.label_name}")
+        print(f"output shapes: {output_shapes}")
+
+        c, self.h ,self.w = (input_shapes[1],input_shapes[2],input_shapes[3])
+
+    def preprocess(self,im):
+        im = cv2.resize(im,(self.h,self.w))
+        im = im / 255.0  # 0 - 255 to 0.0 - 1.0   
+        im = np.transpose(im, (2, 0,1))
+        im = im[np.newaxis,:,:,:]
+        return im
+        
+    def run(self, frame):
+        im = self.preprocess(frame)
+        pred_onx = self.sess.run( [self.label_name], {self.input_name: im.astype(np.float32)})[0]
+        pred_onx = pred_onx[0]                      # x(1, 25200, 7) to x(25200, 7)
+        
+        boxes = np.squeeze(pred_onx[:, :4])       # boxes  [25200, 4]
+        scores = np.squeeze( pred_onx[:, 4:5])    # confidences  [25200, 1]
+        
+        classdata = pred_onx[:, 5:]               # get classes
+        classes = []  # create a list
+        for i in range(classdata.shape[0]):         # loop through all predictions
+            classes.append(classdata[i].argmax())   # get the best classification location
+        classes = np.array(classes)
+        
+        # Convert nx4 boxes from [x, y, w, h] to [x1, y1, x2, y2] where xy1=top-left, xy2=bottom-right
+        xx, yy, ww, hh = boxes[:, 0], boxes[:, 1], boxes[:, 2], boxes[:, 3] #xywh
+        xyxy = np.array([xx - ww / 2, yy - hh / 2, xx + ww / 2, yy + hh / 2])  # xywh to xyxy   [4, 25200]  # coordinate with respect to image model (h,w)
+
+        # --- Data after nms
+        pick = [idx for idx in range(0,len(scores)) if scores[idx] > self.conf]
+        xyxy = xyxy[:,pick] 
+        scores = scores[pick]
+        classes = classes[pick]
+        
+        # -- NMS
+        xyxy, pick = non_max_suppression_fast(xyxy, overlapThresh=0.5)
+        scores = scores[pick]
+        classes = classes[pick]
+
+        res_boxes,res_classes,res_scores, im_draw = self.draw(frame,xyxy,scores,classes)
+
+        return res_boxes,res_classes,res_scores, im_draw
+
+    def draw(self,frame,xyxy,scores,classes):
+        res_boxes = []
+        res_scores = []
+        res_classes =  []
+        iH, iW = (frame.shape[0],frame.shape[1])
+        for i in range(len(scores)):
+            if ((scores[i] > self.conf) and (scores[i] <= 1.0)):
+                
+                # Normalize in range 0-1
+                xmin = (xyxy[0][i] / self.h )
+                ymin = (xyxy[1][i] / self.w )
+                xmax = (xyxy[2][i] / self.h )
+                ymax = (xyxy[3][i] / self.w )
+
+                # Bring 0-1 coordinate in image cooridnates
+                xmin = int(max(1,xmin*iW))
+                ymin = int(max(1,ymin*iH))
+                xmax = int(min(iW,xmax*iW))
+                ymax = int(min(iH,ymax*iH))
+
+                idx = classes[i]
+                score = np.round(scores[i],2)
+                name = self.class_names[idx]
+                print(name,score,[xmin,ymin,xmax,ymax])
+
+                cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), self.class_colors[classes[i]], 2)
+                cv2.putText(frame, name, (max(0,xmin-12),ymin), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1, cv2.LINE_AA)
+
+                res_boxes.append([xmin,ymin,xmax,ymax])
+                res_classes.append(name)
+                res_scores.append(score)
+        return res_boxes,res_classes,res_scores, frame
+
 
 cv2.namedWindow('Image',cv2.WINDOW_NORMAL) 
 #folder_path = "../datasets/dadi/dataset_dadi_sapera"
@@ -81,65 +158,17 @@ cv2.namedWindow('Image',cv2.WINDOW_NORMAL)
 #    filename = os.path.join(folder_path,name)        
 #    print(filename)
 #    frame = cv2.imread(filename, cv2.IMREAD_COLOR)
- 
+
+inf = Inference() 
 cap = cv2.VideoCapture(0)
 while True:
     ret, frame = cap.read()
-    iH, iW = (frame.shape[0],frame.shape[1])
-
-    im = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    im = cv2.resize(im,(h,w))
-    im = im / 255.0  # 0 - 255 to 0.0 - 1.0   
-    im = np.transpose(im, (2, 0,1))
-    im = im[np.newaxis,:,:,:]
-    pred_onx = sess.run( [label_name], {input_name: im.astype(np.float32)})[0]
+    frame= cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     
-    pred_onx = pred_onx[0]                      # x(1, 25200, 7) to x(25200, 7)
-    boxes = np.squeeze(pred_onx[:, :4])       # boxes  [25200, 4]
-    scores = np.squeeze( pred_onx[:, 4:5])    # confidences  [25200, 1]
-    
-    classdata = pred_onx[:, 5:]               # get classes
-    classes = []  # create a list
-    for i in range(classdata.shape[0]):         # loop through all predictions
-        classes.append(classdata[i].argmax())   # get the best classification location
-    classes = np.array(classes)
-    
-    # Convert nx4 boxes from [x, y, w, h] to [x1, y1, x2, y2] where xy1=top-left, xy2=bottom-right
-    xx, yy, ww, hh = boxes[:, 0], boxes[:, 1], boxes[:, 2], boxes[:, 3] #xywh
-    xyxy = np.array([xx - ww / 2, yy - hh / 2, xx + ww / 2, yy + hh / 2])  # xywh to xyxy   [4, 25200]  # coordinate with respect to image model (h,w)
+    res_boxes,res_classes,res_scores, im_draw = inf.run(frame)
 
-    # --- Data after nms
-    pick = [idx for idx in range(0,len(scores)) if scores[idx] > conf]
-    xyxy = xyxy[:,pick] 
-    scores = scores[pick]
-    classes = classes[pick]
-    
-    # -- NMS
-    xyxy, pick = non_max_suppression_fast(xyxy, overlapThresh=0.5)
-    scores = scores[pick]
-    classes = classes[pick]
+    im_draw = cv2.cvtColor(im_draw, cv2.COLOR_RGB2BGR)
 
-    for i in range(len(scores)):
-     
-        if ((scores[i] > conf) and (scores[i] <= 1.0)):
-            
-            # Normalize in range 0-1
-            xmin = (xyxy[0][i] / h )
-            ymin = (xyxy[1][i] / w )
-            xmax = (xyxy[2][i] / h )
-            ymax = (xyxy[3][i] / w )
-
-            # Bring 0-1 coordinate in image cooridnates
-            xmin = int(max(1,xmin*iW))
-            ymin = int(max(1,ymin*iH))
-            xmax = int(min(iW,xmax*iW))
-            ymax = int(min(iH,ymax*iH))
-
-            idx = classes[i]
-            print(class_names[idx],np.round(scores[i],2),[xmin,ymin,xmax,ymax])
-                        
-            cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), class_colors[classes[i]], 2)
-    
-    cv2.imshow("Image", frame)
+    cv2.imshow("Image", im_draw)
     cv2.waitKey(1)
             
